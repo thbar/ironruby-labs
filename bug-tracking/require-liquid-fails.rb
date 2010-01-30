79a6478eaa3fbfc378dd9817a70db83b6b8db5eb
Thibaut Barrère <thibaut.barrere@gmail.com>
2010-01-30 05:10:46 -0700

    Add a comment

--- bug-tracking/require-liquid-fails.rb
+++ bug-tracking/require-liquid-fails.rb
@@ -1,7 +1,12 @@
 $LOAD_PATH << File.dirname(__FILE__) + '/gems/liquid-2.0.0/lib'
+
 require 'liquid'
 
-#puts "We're here running #{RUBY_PLATFORM}"
+# the require above throws:
+# :0:in `require': Expression of type 'System.Boolean' cannot be used for parameter of type 'System.Object' of method 'System.Object DefineMethod(System.Object, IronRuby.Runtime.RubyScope, IronRuby.Runtime.Calls.RubyMethodBody)' (ArgumentError)
+#        from ./gems/liquid-2.0.0/lib/liquid.rb:51
+#        from hello.rb:2
+#        from :0:in `require' 
 
 @template = Liquid::Template.parse("hi from RUBY_PLATFORM {{platform}}")
 puts @template.render( 'platform' => RUBY_PLATFORM)
